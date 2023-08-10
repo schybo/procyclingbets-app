845e3f9d0d44f7c501baa2b433cd13e98fb94bf2
Brent Scheibelhut <brent.scheibelhut@instacart.com>
2023-08-10 06:41:40 -0400

    Clean up login screen

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,33 @@ const App = () => {
   if (!session) {
     console.log("HELLO");
     // return <div>Logged out!</div>;
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+    return (
+      <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a
+            href="#"
+            class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              class="w-8 h-8 mr-2"
+              // src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              src="/assets/icon/icon.png"
+              alt="logo"
+            />
+            Pro Cycling Bets
+          </a>
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space">
+              <Auth
+                supabaseClient={supabase}
+                providers={["google", "github", "facebook", "discord"]}
+                appearance={{ theme: ThemeSupa }}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
   } else {
     console.log("BLAH");
     return (
